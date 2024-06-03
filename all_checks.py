9d2390b80c9ec490ea60c88b7bf6d0cacb6f5f53
yangzheng-brown <yang_zheng2@brown.edu>
2024-06-03 16:49:34 -0400

    modified

--- all_checks.py
+++ all_checks.py
@@ -5,7 +5,7 @@ def check_disk_full(disk, min_gb, min_percent):
     du = shutil.disk_usage(disk)
     # Calculate the percentage of free space
     percent_free = 100 * du.free / du.total
-    # Calculate how many free gigabytes
+    # Calculate how many free gigabytes ffdsafdsafdsafsafsd
     gigabytes_free = du.free / 2**30
     if percent_free < min_percent or gigabytes_free < min_gb:
         return True
